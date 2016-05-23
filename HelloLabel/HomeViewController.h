2f43112ba7c1d9c7ab098eb58e95bdec2bf290ef
邢东飞mac <xingdongfeimac@madeMacBook-Pro.local>
2016-05-23 13:52:22 +0800

    first commit

---
+++ HelloLabel/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  HelloLabel
+//
+//  Created by 邢东飞mac on 16/5/20.
+//  Copyright © 2016年 邢东飞mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface HomeViewController : ViewController
+
+@end
---
+++ HelloLabel/HomeViewController.m
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.m
+//  HelloLabel
+//
+//  Created by 邢东飞mac on 16/5/20.
+//  Copyright © 2016年 邢东飞mac. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@implementation HomeViewController
+
+@end
